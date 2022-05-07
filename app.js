daf9c0ad1ca026e2588b2e2e68d7b1db289b3595
Brayan Riascos <briascosarroyo@gmail.com>
2022-05-06 23:32:28 -0500

    segundo commit

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const URL="https://rickandmortyapi.com/api/character";
 const UR='https://rickandmortyapi.com/api/character/?name=';
+const nameC= document.getElementById("nameC");
+
 
 window.addEventListener("load", getCharacters());
-
+nameC.addEventListener('keyup', checkCharacter());
 
 
 
@@ -11,12 +13,12 @@ function getCharacters(){
     .then(response=>response.json())
     .then(data=>{
         data.results.forEach(element => {
-            iterar(element.name, element.image);
+            createElements(element.name, element.image);
         }); 
     });     
 }
 
-function iterar(name, img){
+function createElements(name, img){
     const card=document.getElementById("card");
     const ContainerCard=document.createElement("div");
     const imaCard=document.createElement("img");
@@ -38,6 +40,21 @@ function iterar(name, img){
     return card;   
 }
 
-function buscar(){
-
+function checkCharacter(){
+    while(Card.lastChild){
+        Card.removeChild(Card.lastChild)
+    }
+    getCharactersByKeys(nameC.value);  
 }
+
+function getCharactersByKeys(searchCharacter){
+    fetch(UR+searchCharacter)
+    .then(response => response.json())
+     .then(data =>{
+        data.results.forEach(element =>{
+            createElements(element.name, element.image)
+            console.log(element.name)
+                                          
+         });
+     })
+    }
